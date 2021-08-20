4c6d81decedd4c1c5cf8de0203a89637952354c7
4dsolutions <kirby.urner@gmail.com>
2021-08-20 09:54:53 -0700

    phi and pi worksheets

---
+++ gen_phi.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  3 16:08:49 2015
+Modified Oct 17, 2017
+Modified Feb 27, 2020
+
+@author: Kirby Urner
+
+Another generator example:  converging to Phi
+
+https://oeis.org/A000045
+http://www.maths.surrey.ac.uk/hosted-sites/R.Knott/Fibonacci/phi.html#section7
+
+Shows eval( ) in action
+
+"""
+
+from decimal import Decimal, getcontext
+
+getcontext().prec = 35
+
+one = Decimal('1')
+five = Decimal('5')
+check = (one + five.sqrt())/2
+
+def continuedint():
+    
+    patt = "+ 1/(1"
+    the_expr = ''  # empty
+    n = 1
+    
+    while True:
+        the_expr = the_expr + patt
+        the_frac = "1 " + (the_expr + ")" * n)
+        
+        # print(the_frac)
+        yield the_frac  # or show the_frac
+        
+        n += 1
+        
+def continued():
+    
+    patt = "+ one/(one"
+    the_expr = ''  # empty
+    n = 1
+    
+    while True:
+        the_expr = the_expr + patt
+        the_frac = "1 " + (the_expr + ")" * n)
+        
+        # print(the_frac)
+        yield eval(the_frac)  # or show the_frac
+        
+        n += 1
+             
+def test_me():
+    the_gen = continued()
+    last_time = 1
+    while True:
+        approx = next(the_gen)
+        print(approx)
+        if approx == check:
+            break
+        if abs(approx - last_time) < Decimal(1e-30): 
+            break
+        last_time = approx
+
+    print("Approx:", approx)
+    print("Check :", check)
+    
+
+  
+  
+ 
---
+++ gen_pi.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  3 16:08:49 2015
+
+@author: kurner
+
+Another generator example:  converging to Pi
+
+https://mail.python.org/pipermail/edu-sig/2015-September/date.html
+
+"""
+
+def pi():
+
+    k, a, b, a1, b1 = 2, 4, 1, 12, 4
+    while True:
+        p, q, k = k*k, 2*k+1, k+1
+        a, b, a1, b1 = a1, b1, p*a+q*a1, p*b+q*b1
+        d, d1 = a/b, a1/b1
+        while d == d1:
+            yield int(d)
+            a, a1 = 10*(a%b), 10*(a1%b1)
+            d, d1 = a/b, a1/b1
+#%%
+if __name__ == "__main__":                    
+    the_gen = pi()
+    num = 0
+    for digit in the_gen:
+        print(digit,end="")
+        if num > 1000:
+            break
+        num = num + 1
+       
+    print()
