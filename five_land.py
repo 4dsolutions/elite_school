e26db9f3942c47267d16defb1f135afa674de34c
4dsolutions <kirby.urner@gmail.com>
2022-03-05 07:08:26 -0800

    five_land module

--- five_land.py
+++ five_land.py
@@ -6,14 +6,19 @@ Created on Sun Feb 27 15:25:48 2022
 @author: Kirby Urner
 """
 
+words = set()
 from string import ascii_lowercase
+
 def initialize():
     global words
     if not words:
         with open("wordkeep.txt", "r") as fileobj:
             words = set(fileobj.read().split("\n"))
+    if "" in words:
+        words.remove("")
+    print(len(words))
             
-def roll_alpha(the_word):
+def roll_alpha(the_word, show_candidate=False):
     fish = set()
     fish.add(the_word)
     stone = list(the_word)
@@ -24,17 +29,91 @@ def roll_alpha(the_word):
             candidate = "".join(listy)
             if candidate in words:
                 fish.add(candidate)
+                if show_candidate:
+                    print(candidate)
 
     fish.remove(the_word)
     return fish
 
 def fish_pond(pond):
-    bigger_pond = set()
+    """
+    Find all the words one letter distant from 
+    all of those in the current set
+    """
+    bigger_pond = set(pond)
     for fish in pond:
         kin = roll_alpha(fish)
         bigger_pond.update(kin)
     return bigger_pond
 
+def grow_pool(p, show_stats=False):
+    """
+    find all the words reachable from p by means 
+    of *any number* of one letter legal word hops,
+    and stop when p stops growing.
+    """
+    new_p = fish_pond(p)
+    while len(new_p) > len(p):
+        p = new_p
+        new_p = fish_pond(p)
+        if show_stats:
+            print(len(p), len(new_p))
+    return p
+
+def survey(curate_len=0):
+    """
+    find the biggest island starting from never_reached
+    words. How big is the biggest island.
+    """
+    global final_set, never_reached
+    biggest = 0
+    curated = set()
+    biggest_word = ""
+    if (not "final_set" in globals() 
+        or not "never_reached" in globals()
+        or len(final_set)==0 
+        or len(never_reached)==0):
+        # regenerate on the fly
+        final_set = grow_pool({"caked"})
+        never_reached = words - final_set
+    tally = dict()
+    for w in never_reached:
+        neighborhood = grow_pool({w})
+        nb_size = len(neighborhood)
+        if curate_len == nb_size:
+            curated.add(w)
+        if nb_size > biggest:
+            biggest = nb_size
+            biggest_word = w
+        tally[len(neighborhood)] = tally.get(len(neighborhood), 0) + 1
+    return tally, biggest, biggest_word, curated
+
+
+def path_exists(wordA: str, wordB: str) -> bool:
+    """
+    yes or no, does a path exist between these two input words?
+    """
+    if wordA not in words or wordB not in words:
+        raise ValueError("not both legal words")
+    if wordA == wordB:
+        return True
+
+    # interim list prevents fragmentation into characters
+    poolA = set([wordA])
+    poolB = set([wordB])
+
+    for _ in range(4):
+        poolA = fish_pond(poolA)
+        poolB = fish_pond(poolB)
+
+    if len(poolA) > 24 and len(poolB) > 24:
+        print("Both on big island")
+        return True
+    if len(poolA.intersection(poolB)) > 0:
+        print("Same small island")
+        return True
+    return False
+  
 def word_path(start_word):
     visited = set(start_word)
     last_word = start_word
